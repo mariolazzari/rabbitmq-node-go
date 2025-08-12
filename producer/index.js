92083965afb66ef1049c4213438a496b8390f75c
Mario Lazzari <mario.lazzari@gmail.com>
2025-08-12 11:29:21 +0200

    feat: producer

---
+++ producer/index.js
@@ -0,0 +1,20 @@
+const amqp = require("amqplib");
+
+// produce messages to a classic queue
+async function produce() {
+  // physical connection
+  const connection = await amqp.connect("amqp://guest:guest@localhost:5672/");
+  // virtual connection
+  const channel = await connection.createChannel();
+  // queue name
+  const queue = "hello";
+  // var incremented every second
+  const count = 0;
+  setInterval(() => {
+    const message = `Message ${count}`;
+    channel.sendToQueue(queue, Buffer.from(message));
+    console.log(`[x] Sent ${message}`);
+  }, 1000);
+}
+
+produce();
